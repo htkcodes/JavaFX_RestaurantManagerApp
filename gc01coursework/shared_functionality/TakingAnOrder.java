09e91c4d3da15aef7fceed12406145b28e5e0a3e
Rachel Slater <rachelrobynslater@gmail.com>
2016-12-05 20:33:24 +0000

    xml file now generated for saved orders

--- gc01coursework/shared_functionality/TakingAnOrder.java
+++ gc01coursework/shared_functionality/TakingAnOrder.java
@@ -15,6 +15,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import com.sun.org.apache.xml.internal.serialize.*;
+
 public class TakingAnOrder implements Initializable {
 	private String tableClicked;
 	
@@ -29,6 +40,15 @@ public class TakingAnOrder implements Initializable {
 
 	
 	public TakingAnOrder(String tableNum) {
+		try {
+			this.saveOrder();
+		} catch(ParserConfigurationException parse) {
+			
+		} catch(FileNotFoundException fileNotFound) {
+			
+		} catch (IOException ioexception) {
+			
+		}
 	}
 
 
@@ -55,8 +75,72 @@ public class TakingAnOrder implements Initializable {
 	}
 	
 	@FXML
-	private void saveOrder() {
+	private void saveOrder() throws ParserConfigurationException, IOException {
 		
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		Document xmlDoc = docBuilder.newDocument();	//Now we've created the document, we're ready to build the XML.
+		
+		//XML Elements and Text Nodes:
+		// 	<orders>
+		//		<order>
+		//			<tablenumber>Table Number</tablenumber>
+		//			<date>Date</date>
+		//			<items>Ordered Items</items>
+		//			<totalcost>Total Cost</totalcost>
+		//			<comments>Comments</comments>
+		//			<specialrequest>Special Requests</specialrequests>
+		//		<order>
+		//</orders>
+		
+		Element rootElement = xmlDoc.createElement("orders");
+		Element mainElement = xmlDoc.createElement("order");
+		Text tableNumberText = xmlDoc.createTextNode("Table Number");
+		Element tableNumber = xmlDoc.createElement("tablenumber");		 
+		Text dateText = xmlDoc.createTextNode("Date");
+		Element date = xmlDoc.createElement("date");	
+		
+		//Now we just append all the children.
+		
+		tableNumber.appendChild(tableNumberText);
+		date.appendChild(dateText);
+		
+		mainElement.appendChild(tableNumber);
+		mainElement.appendChild(date);
+		
+		rootElement.appendChild(mainElement);
+		
+		xmlDoc.appendChild(rootElement);
+		
+		//Now we need to set the output format. 
+		OutputFormat outFormat = new OutputFormat(xmlDoc);
+		outFormat.setIndenting(true);
+		
+		//Declare the file:
+		File xmlFile = new File("./orders.xml");
+		FileOutputStream outStream = new FileOutputStream(xmlFile);
+		
+		//Serialize XML data with the specified OutputStream:
+		XMLSerializer serializer = new XMLSerializer(outStream, outFormat);
+		serializer.serialize(xmlDoc);
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
