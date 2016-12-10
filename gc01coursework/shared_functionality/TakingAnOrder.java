a1caa54110b815bd17030d84a50a4e6378b1a9cf
Rachel Slater <rachelrobynslater@gmail.com>
2016-12-10 13:02:26 +0000

    view menu functionality for orders added

--- gc01coursework/shared_functionality/TakingAnOrder.java
+++ gc01coursework/shared_functionality/TakingAnOrder.java
@@ -5,19 +5,22 @@ import java.io.IOException;
 import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.ResourceBundle;
 import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import gc01coursework.admin_functionality.EditTheMenu;
 import gc01coursework.users_and_login.Supervisor;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
@@ -53,9 +56,12 @@ import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Element;
 import java.io.File;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 
 public class TakingAnOrder implements Initializable {
 	private String tableClicked;
+	private ObservableList<String> starterItems;
 
 	@FXML
 	private GridPane orderGridPane;
@@ -69,8 +75,18 @@ public class TakingAnOrder implements Initializable {
 	private TextField comments;
 	@FXML
 	private Button saveOrderButton;
+	
+	//Inside Items Panel:
 	@FXML
-	private Button viewMenuButton;
+	private GridPane menuGridPane;
+	@FXML
+	private Label theItems;
+	@FXML
+	private ListView<String> starterList;
+	@FXML
+	private Button selectStarter;
+	@FXML
+	private Button removeStarter;
 	
 	public TakingAnOrder(String tableNum) {
 	}
@@ -86,11 +102,47 @@ public class TakingAnOrder implements Initializable {
 	private void setTableClicked(String tableClicked) {
 		this.tableClicked = tableClicked;
 	}
+	
+	@FXML
+	private void getStarters() throws ParserConfigurationException, SAXException, IOException {
+		ArrayList<String> allStarters = new ArrayList<String>();
 
+		File file = new File("starters.xml");
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		Document doc = documentBuilder.parse(file);
+
+		doc.getDocumentElement().normalize();
+		NodeList nList = doc.getElementsByTagName("starter");
+
+		for (int i = 0; i < nList.getLength(); i++) {
+			Node nNode = nList.item(i);
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+				String starterName = eElement.getElementsByTagName("starterName").item(0).getTextContent();
+				String starterPrice = eElement.getElementsByTagName("starterPrice").item(0).getTextContent();
+				String fullStarter = starterName + " - £" + starterPrice;
+				allStarters.add(fullStarter);
+			}
+			
+			ObservableList<String> starterItems = FXCollections.observableArrayList(allStarters);
+			System.out.println(starterItems);
+			ListView<String> starterList = new ListView<String>(starterItems);
+			
+			menuGridPane.add(starterList, 1, 0);
+		}
+	}
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		tableNumber.setText(getTableClicked());
-
+		
+		try {
+			getStarters();
+		} catch (ParserConfigurationException | SAXException | IOException e) {
+			e.printStackTrace();
+		}
+		
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Date date = new Date();
 		String now = (dateFormat.format(date)).toString();
@@ -172,34 +224,6 @@ public class TakingAnOrder implements Initializable {
 		System.out.println("\nXML DOM Created Successfully..");
 	}
 
-
-	/**
-	 * Rendering menu.xml so new order items can be added!
-	 * @throws IOException 
-	 * @throws SAXException 
-	 * @throws ParserConfigurationException 
-	 *
-	 * 
-	 * 
-	 */
-
-	@FXML
-	public void getMenuXMLData() throws IOException, ParserConfigurationException, SAXException {
-
-		ViewTheMenu menuViewer = new ViewTheMenu();
-		Stage menuView = new Stage();
-		FXMLLoader loaderMenu = new FXMLLoader();
-		loaderMenu.setLocation(getClass().getResource("../shared_functionality/viewMenu.fxml"));
-		loaderMenu.setController(menuViewer);
-		Parent viewTheMenu = (Parent)loaderMenu.load();
-		Scene scene = new Scene(viewTheMenu);
-		menuView.setTitle("Rachel's Restaurant Menu");
-		menuView.initModality(Modality.APPLICATION_MODAL);
-		menuView.initOwner(viewMenuButton.getScene().getWindow());
-		menuView.setScene(scene);
-		
-		menuView.showAndWait();
-	}
 }
 
 
